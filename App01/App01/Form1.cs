963233ac39e5e71dd36131978ce32a650d40fd9e
Ali Baris Cikmaz <152120191012@ogrenci.ogu.edu.tr>
2022-03-27 14:01:48 +0300

    I added the default username and passwords with the login functionality.

--- App01/App01/Form1.cs
+++ App01/App01/Form1.cs
@@ -9,6 +9,24 @@ namespace App01
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            var defaultUsernames = new[] { "admin", "user" };
+            var defaultPasswds = new[] { "admin", "user" };
+            bool loginFlag = false;
+
+            for (int i = 0; i < defaultUsernames.Length; i++){
+                if ((defaultUsernames[i] == txtUsernameEntry.Text) && (defaultPasswds[i] == txtPasswordEntry.Text)) {
+                    loginFlag = true;
+                    break;
+                }
+            }
+
+            if (loginFlag)
+            {
+                // ---------- This part will lead to the main menu part --------------------
+            }
+
+            else
+                System.Windows.Forms.MessageBox.Show("Invalid username or password, please try again!", "TRY AGAIN", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
 
         }
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -17,6 +35,7 @@ namespace App01
             {
                 btnLogin.PerformClick();
             }
+
             return base.ProcessCmdKey(ref msg, keyData);
         }
     }
