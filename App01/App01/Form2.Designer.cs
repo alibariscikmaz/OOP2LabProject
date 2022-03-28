e74164464aaafa7212f0f8f0cc534f363662ab3a
Hüseyin Koçer <huso1475@msn.com>
2022-03-28 03:00:05 +0300

    added custom difficulty input boxes, shapes container and save button which records the selected options to a txt file and whenever the form opens it gets the data from this txt file.

--- App01/App01/Form2.Designer.cs
+++ App01/App01/Form2.Designer.cs
@@ -31,12 +31,24 @@
             this.components = new System.ComponentModel.Container();
             this.settingsbutton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Difficulty = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.Difficulty.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // settingsbutton
@@ -52,9 +64,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.checkedListBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.Difficulty);
             this.panel1.Location = new System.Drawing.Point(107, 22);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(681, 416);
@@ -62,46 +75,130 @@
             this.panel1.Visible = false;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(497, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Changes Saved Succesfully";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 165);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 194);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size";
+            // 
             // checkedListBox1
             // 
-            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.checkedListBox1.CheckOnClick = true;
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
-            "Hard",
-            "Normal",
-            "Easy",
-            "Custom"});
-            this.checkedListBox1.Location = new System.Drawing.Point(50, 48);
+            "Square",
+            "Triangle",
+            "Circle"});
+            this.checkedListBox1.Location = new System.Drawing.Point(16, 24);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(86, 72);
+            this.checkedListBox1.Size = new System.Drawing.Size(206, 148);
             this.checkedListBox1.TabIndex = 0;
-            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Difficulty
+            // 
+            this.Difficulty.Controls.Add(this.radioButton2);
+            this.Difficulty.Controls.Add(this.numericUpDown2);
+            this.Difficulty.Controls.Add(this.radioButton4);
+            this.Difficulty.Controls.Add(this.numericUpDown1);
+            this.Difficulty.Controls.Add(this.radioButton1);
+            this.Difficulty.Controls.Add(this.radioButton3);
+            this.Difficulty.Location = new System.Drawing.Point(25, 21);
+            this.Difficulty.Name = "Difficulty";
+            this.Difficulty.Size = new System.Drawing.Size(240, 129);
+            this.Difficulty.TabIndex = 9;
+            this.Difficulty.TabStop = false;
+            this.Difficulty.Text = "Difficulty";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 19);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Normal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(165, 99);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(57, 23);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(16, 99);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(67, 19);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Custom";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(99, 99);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 23);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(51, 19);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Hard";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(16, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 19);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Easy";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Difficulty";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 153);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Shape";
             // 
             // Form2
             // 
@@ -115,6 +212,11 @@
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.Difficulty.ResumeLayout(false);
+            this.Difficulty.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -125,8 +227,16 @@
         private Panel panel1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private ContextMenuStrip contextMenuStrip1;
-        private CheckedListBox checkedListBox1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private GroupBox Difficulty;
+        private RadioButton radioButton2;
+        private RadioButton radioButton4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton3;
+        private GroupBox groupBox1;
         private Label label1;
-        private Label label2;
+        private Button button1;
+        private CheckedListBox checkedListBox1;
     }
 }
--- App01/App01/Form2.cs
+++ App01/App01/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace App01
 {
@@ -15,8 +16,9 @@ namespace App01
         public Form2()
         {
             InitializeComponent();
+            config();
         }
-
+        
         private void Form2_Load(object sender, EventArgs e)
         {
 
@@ -24,6 +26,7 @@ namespace App01
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             if (panel1.Visible == false)
             {
                 panel1.Visible = true;
@@ -39,23 +42,92 @@ namespace App01
 
         }
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int index = checkedListBox1.SelectedIndex;
-            int count = checkedListBox1.Items.Count;//get checked item count
-            for (int i = 0; i < count; i++)
-            {
-                //uncheck other items when another item is checked
-                if (index != i)
-                {
-                    checkedListBox1.SetItemChecked(i, false);
-                }
-            }
-        }
+        
 
         private void label1_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            
+            //gets project's \bin\debug directory
+            string currentpath = System.IO.Directory.GetCurrentDirectory();
+            StreamWriter txt = new StreamWriter(currentpath+"\\config.txt");
+            if (radioButton1.Checked) txt.WriteLine("Hard");
+            else txt.WriteLine("empty");
+            if (radioButton2.Checked) txt.WriteLine("Normal");
+            else txt.WriteLine("empty");
+            if (radioButton3.Checked) txt.WriteLine("Easy");
+            else txt.WriteLine("empty");
+            if (radioButton4.Checked)
+            {
+                txt.WriteLine("Custom");
+                txt.WriteLine(numericUpDown1.Value);
+                txt.WriteLine(numericUpDown2.Value);
+            }
+            else 
+            {
+                txt.WriteLine("empty");
+                txt.WriteLine("empty");
+                txt.WriteLine("empty");
+            }
+
+            if (checkedListBox1.GetItemCheckState(0) == CheckState.Checked) txt.WriteLine("Square");
+            else txt.WriteLine("empty");
+            if (checkedListBox1.GetItemCheckState(1) == CheckState.Checked) txt.WriteLine("Triangle");
+            else txt.WriteLine("empty");
+            if (checkedListBox1.GetItemCheckState(2) == CheckState.Checked) txt.WriteLine("Circle");
+            else txt.WriteLine("empty");
+
+            txt.Close();
+            
+        }
+        private void config()
+        {
+            string currentpath = System.IO.Directory.GetCurrentDirectory();
+            StreamReader txt = new StreamReader(currentpath + "\\config.txt");
+            while (!txt.EndOfStream)
+            {
+                if (txt.ReadLine() == "Hard") radioButton1.Checked = true;
+                if (txt.ReadLine() == "Normal") radioButton2.Checked = true;
+                if (txt.ReadLine() =="Easy") radioButton3.Checked = true;
+                if (txt.ReadLine() == "Custom")
+                {
+                    radioButton4.Checked = true;
+                    numericUpDown1.Value = Int32.Parse(txt.ReadLine());
+                    numericUpDown2.Value = Int32.Parse(txt.ReadLine());
+                }
+                else
+                {
+                    txt.ReadLine();
+                    txt.ReadLine();
+                }
+               
+
+                if (txt.ReadLine() == "Square") checkedListBox1.SetItemChecked(0,true);
+                if (txt.ReadLine() == "Triangle") checkedListBox1.SetItemChecked(1, true);
+                if (txt.ReadLine() == "Circle") checkedListBox1.SetItemChecked(2, true);
+                
+
+            }
+            txt.Close();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton4.Checked == true)
+            {
+                numericUpDown1.Visible = true;
+                numericUpDown2.Visible = true;
+            }
+            else
+            {
+                numericUpDown1.Visible = false;
+                numericUpDown2.Visible = false;
+            }
         }
     }
 }
